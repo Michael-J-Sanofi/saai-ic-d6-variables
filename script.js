69f5058038355e1516aa3c002b81e07ec4b3f5b3
Michael-J-Sanofi <139016292+Michael-J-Sanofi@users.noreply.github.com>
2024-11-08 04:11:45 +0000

    followed the lab

---
+++ script.js
@@ -0,0 +1,18 @@
+let animal = "red panda"
+
+console.log("awesome i'm not writing javascript!")
+console.log("I'm writing a second console log")
+console.log(animal)
+console.log("I'm still here...")
+
+let myNameIs = "Michael Pena"
+let age = 52
+let statusSingle = true
+console.log(myNameIs)
+console.log(age)
+console.log(statusSingle)
+let purchasePrice = 20
+
+console.log("Initial purchase price is", purchasePrice)
+let purchasePrice = 10
+console.log("Purchase price during sale is ", purchasePrice)
